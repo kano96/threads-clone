357183a794aae76f8cc51fd6c1a52327f34ad706
Kevin Torres <kevin_akd@hotmail.com>
2023-08-08 17:03:56 -0500

    add bottom and sidebar

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -3,6 +3,7 @@ import { sidebarLinks } from "@/constants";
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
+import { SignedIn, SignOutButton } from "@clerk/nextjs";
 
 function LeftSidebar() {
   const router = useRouter();
@@ -31,6 +32,21 @@ function LeftSidebar() {
             </Link>
           );
         })}
+      </div>
+      <div className="mt-10 px-6">
+        <SignedIn>
+          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
+            <div className="flex cursor-pointer gap-4 p-4">
+              <Image
+                src="/assets/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+              />
+              <p className="text-light-2 max-lg:hidden">Logout</p>
+            </div>
+          </SignOutButton>
+        </SignedIn>
       </div>
     </section>
   );
