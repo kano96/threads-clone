13bb3716b934523dd4de4c1f19d5c0c48777edb1
Kevin Torres <kevin_akd@hotmail.com>
2023-08-09 20:49:07 -0500

    add left and right bar

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -18,7 +18,7 @@ function Bottombar() {
             <Link
               href={link.route}
               key={link.label}
-              className={`leftsidebar_link ${isActive && "bg-primary-500"}`}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
             >
               <Image
                 src={link.imgURL}
@@ -26,6 +26,9 @@ function Bottombar() {
                 width={24}
                 height={24}
               />
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                {link.label.split(/\s+/)[0]}
+              </p>
             </Link>
           );
         })}
