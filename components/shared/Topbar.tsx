a04884172b344cb398194cd99f90845ae7a35363
Kevin Torres <kevin_akd@hotmail.com>
2023-08-09 22:58:04 -0500

    add onboarding page, rightsidebar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
-import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { OrganizationSwitcher } from "@clerk/nextjs";
 
 function Topbar() {
   return (
@@ -29,6 +31,7 @@ function Topbar() {
         </div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
