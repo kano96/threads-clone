ed3f0d6dcbf3f5488136ad18b463d7238411abad
Kevin Torres <kevin_akd@hotmail.com>
2023-08-15 17:36:07 -0500

    add create posts and thread card

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -35,3 +35,41 @@ export async function createThread({
     throw new Error(`Error creating thread: ${error.message}`);
   }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  try {
+    connectToDB();
+
+    // Calculate the number of posts to skip
+    const skips = pageSize * (pageNumber - 1);
+
+    // Fetch posts that have no parents (top level posts)
+    const threadsQuery = Thread.find({
+      parentId: { $in: [null, undefined] },
+    })
+      .sort({ createdAt: "desc" })
+      .skip(skips)
+      .limit(pageSize)
+      .populate({ path: "author", model: User })
+      .populate({
+        path: "children",
+        populate: {
+          path: "author",
+          model: User,
+          select: "_id name parentId image",
+        },
+      });
+
+    const totalThreads = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    });
+
+    const posts = await threadsQuery.exec();
+
+    const isNext = totalThreads > skips + posts.length;
+
+    return { posts, isNext };
+  } catch (error: any) {
+    throw new Error(`Error fetching threads: ${error.message}`);
+  }
+}
