156ce947eb13711292f411a9f09b2605a86e5063
Kevin Torres <kevin_akd@hotmail.com>
2023-08-28 11:11:59 -0500

    add activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -23,19 +23,31 @@ async function Page() {
       <section className="mt-10 flex flex-col gap-5">
         {activity.length > 0 ? (
           <>
-            {activity.map((act: { _id: string; author: { image: string } }) => (
-              <Link key={act._id} href={`/thread/${activity.parentId}`}>
-                <article className="activity-card">
-                  <Image
-                    src={act.author.image}
-                    alt="Profile Picture"
-                    width={20}
-                    height={20}
-                    className="rounded-full object-cover"
-                  />
-                </article>
-              </Link>
-            ))}
+            {activity.map(
+              (act: {
+                _id: string;
+                parentId: string;
+                author: { image: string; name: string };
+              }) => (
+                <Link key={act._id} href={`/thread/${act.parentId}`}>
+                  <article className="activity-card">
+                    <Image
+                      src={act.author.image}
+                      alt="Profile Picture"
+                      width={20}
+                      height={20}
+                      className="rounded-full object-cover"
+                    />
+                    <p className="!text-small-regular text-light-1">
+                      <span className="mr-1 text-primary-500">
+                        {act.author.name}
+                      </span>{" "}
+                      replied to your thread
+                    </p>
+                  </article>
+                </Link>
+              )
+            )}
           </>
         ) : (
           <p className="!text-base-regular text-light-3">No activity yet</p>
